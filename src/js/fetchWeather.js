e2515143346aa6bdc21a8f2e271aaa92ee45a623
OlhaLu <olhalutsyshyna@gmail.com>
2019-07-17 14:35:58 +0300

    Fix http

--- src/js/fetchWeather.js
+++ src/js/fetchWeather.js
@@ -1,5 +1,5 @@
 export default function fetchLocalWeather(query) {
-  const baseUrl = 'http://api.apixu.com/v1';
+  const baseUrl = 'https://api.apixu.com/v1';
   const resource = '/current.json';
   const reqParams = `?key=78f1a6c1d8a84e2fa2d152923192506&q=${query}`;
 
